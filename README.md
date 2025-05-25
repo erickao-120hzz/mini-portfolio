88d89042206f9f6febd2d75256b9b490a9c67ff0
Erick Carvalho <erickcarvalho661@hotmail.com>
2025-05-25 20:14:10 -0300

    modificado README pelo VS Code

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# mini-portfolio
+# Mini Portfólio - Erick de Carvalho
+
+🚀 Projeto pessoal criado com HTML, CSS e JavaScript para apresentar meu portfólio como Desenvolvedor Web.
+
+🔗 **Acesse o projeto online:**  
+👉 [Clique aqui para ver o portfólio](https://erickao-120hzz.github.io/mini-portfolio/)
+
+## ✨ Tecnologias utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## 💻 Sobre o projeto
+
+Este projeto foi feito com o objetivo de praticar meus conhecimentos em desenvolvimento web e criar uma mini apresentação pessoal online. Ele conta com:
+
+- Informações sobre mim e meus hobbies
+- Links para minhas redes sociais
+- Visual responsivo e animado
+
+## 📸 Preview
+
+![Preview do Projeto](./src/imagens/preview-portfolio.jpg)
+
+## 📱 Minhas redes
+
+- [LinkedIn](https://www.linkedin.com/in/erick-carvalho-394860243/)
+- [GitHub](https://github.com/erickao-120hzz)
+- [Instagram](https://www.instagram.com/erickzdv)
+- [Facebook](https://www.facebook.com/ErickCarvalho26)
+- [Discord](https://discord.com/invite/YfDZbKJVt5)
+
+---
+
+🧠 Em constante aprendizado e evolução. Em breve, novas versões com:
+
+- TypeScript
+- React
+- Node.js
+- SQL
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,29 @@
-/* 
-
-
-*/
-
-const abas = document.querySelector(".aba");
+const abas = document.querySelectorAll(".aba"); // Corrigido para All
 
 abas.forEach(aba => {
-    abas.addEventListener("click", function () {
+    aba.addEventListener("click", function () {
 
         if (aba.classList.contains("selecionado")) {
             return;
         }
 
-        selecionarAba(aba)
-        mostrarInformacoesDaAba(aba)
+        selecionarAba(aba);
+        mostrarInformacoesDaAba(aba);
     });
 });
 
 function selecionarAba(aba) {
     const abaSelecionada = document.querySelector(".aba.selecionado");
-    aba.Selecionada.classList.remove("selecionado");
+    abaSelecionada.classList.remove("selecionado");
 
     aba.classList.add("selecionado");
-
 }
 
 function mostrarInformacoesDaAba(aba) {
     const informacaoSelecionada = document.querySelector(".informacao.selecionado");
     informacaoSelecionada.classList.remove("selecionado");
 
-    const idDoElementoDeinformacoesDaAba = `informacao-${aba.id}`
-
-    const informacaoASerMostrada = document.getElementById();
+    const idDoElementoDeinformacoesDaAba = `informacao-${aba.id}`;
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeinformacoesDaAba);
     informacaoASerMostrada.classList.add("selecionado");
-
-
 }
